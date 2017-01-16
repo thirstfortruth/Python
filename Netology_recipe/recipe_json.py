57d6ff46eb5988d3d2c9829f8c449d365a35f83d
thirstfortruth <thirstfortruth@yandex.ru>
2017-01-17 00:08:00 +0300

    Netology - working with different datatypes

---
+++ Netology_recipe/recipe_json.py
@@ -0,0 +1,6 @@
+import json
+file = input('Please enter filename:')
+#'D:/Python/GIT_HUB_repository/Netology_recipe/recipes.json'
+with open(file) as json_data_file:
+    data = json.load(json_data_file)
+print(data)
