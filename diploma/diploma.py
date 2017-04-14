ad20ff18ffa9f157ec3f4a51bac783e521bc10b5
thirstfortruth <thirstfortruth@yandex.ru>
2017-04-15 01:12:02 +0300

    Diploma. First version of partial extract

--- diploma/diploma.py
+++ diploma/diploma.py
@@ -3,7 +3,7 @@ from urllib.parse import urlencode, urlparse
 import requests
 import math
 import sys
-import collections
+import datetime
 import pandas as pd
 import numpy as np
 
@@ -100,11 +100,9 @@ def get_followers_groups(followers, followers_count):
                                      'count': 1000,
                                      'v': VERSION}
                 response_groups_get = requests.get('https://api.vk.com/method/groups.get', params_groups_get).json()
-                #print(response_groups_get.json())
                 groups = groups + response_groups_get['response']['items']
                 time.sleep(0.3)
             except:
-                #print("response_groups_get, response: ", response_groups_get)
                 time.sleep(0.3)
     else:
         try:
@@ -150,6 +148,7 @@ def get_top_list(input_list, limit):
 
 
 def get_followers_subquery(user_id):
+    print('\nStart getting followers at: ', datetime.datetime.now())
     followers = []
     current_count = 0
     count = 1000
@@ -162,7 +161,7 @@ def get_followers_subquery(user_id):
     response_followers_count = requests.get('https://api.vk.com/method/users.get', params_followers_count).json()
     followers_count = response_followers_count["response"][0]["followers_count"]
     upper_limit = (math.ceil(followers_count/(offset_step*exec_limit)))*(offset_step*exec_limit)
-    upper_limit=26000
+    # upper_limit=26000
     while current_count < upper_limit:
         progress_bar(current_count, upper_limit)
         code = '''var count = ''' + str(count) + ''';
@@ -189,22 +188,20 @@ def get_followers_subquery(user_id):
                           "code": code,
                           "v": VERSION}
         response_followers = requests.post('https://api.vk.com/method/execute', execute_params).json()
-        #print(response_followers["response"])
         followers = followers + response_followers["response"]
-        time.sleep(0.3)
+        # time.sleep(0.3)
     progress_bar(upper_limit, upper_limit)
     return followers
 
 
 def get_users_groups_subquery(users_list):
-    users_groups = {}
+    print('\nStart getting groups at: ', datetime.datetime.now())
     exec_limit = 25
     counter = 0
     upper_limit = len(users_list)
     while counter < upper_limit:
         progress_bar(counter, upper_limit)
         temp_list = [list(x.keys())[0] for x in users_list[counter:counter+exec_limit]]
-        #print(temp_list)
         code = '''var user_list = ''' + str(temp_list) + ''';
                 var version = ''' + VERSION + ''';
                 var token = "''' + access_token + '''";
@@ -223,16 +220,35 @@ def get_users_groups_subquery(users_list):
         execute_params = {"access_token": access_token,
                           "code": code,
                           "v": VERSION}
-        response_groups = requests.post('https://api.vk.com/method/execute', execute_params).json()["response"]
-        #print(response_groups)
-        # print(list(range(len(temp_list))))
-        for i in range(len(temp_list)):
-            list(users_list[counter + i].values())[0]['groups'] = response_groups[i]
-        # users_list = users_groups + response_groups
-        counter += exec_limit
-        #time.sleep(0.3)
-        progress_bar(upper_limit, upper_limit)
-    return users_groups
+        try:
+            response_groups = requests.post('https://api.vk.com/method/execute', execute_params).json()["response"]
+            for i in range(len(temp_list)):
+                list(users_list[counter + i].values())[0]['groups'] = response_groups[i]
+            counter += exec_limit
+            progress_bar(upper_limit, upper_limit)
+        except:
+            pass
+    return users_list
+
+
+def save_results(details_to_save, file_to_save):
+    print("Saving to file {}".format(file_to_save))
+    with open(file_to_save, 'w') as f:
+        f.write("USER_ID;SEX;BIRTH_DATE;GROUP_NO" + "\n")
+    with open(file_to_save, 'a') as f:
+        for user in details_to_save:
+            groups = list(user.values())[0]['groups']
+            if groups:
+                for group in groups:
+                    string_to_write = str(list(user.keys())[0]) + \
+                                      ';' + \
+                                      str(list(user.values())[0]['sex']) + \
+                                      ';' + \
+                                      str(list(user.values())[0]['bdate']) + \
+                                      ';' + \
+                                      str(group)
+                    f.write(string_to_write+"\n")
+
 # print('\nGetting followers...')
 # # followers = get_followers(USER_ID)
 # # friends = get_friends(USER_ID)
@@ -248,7 +264,6 @@ def get_users_groups_subquery(users_list):
 # top_groups = get_top_list(groups, 100)
 # print(get_groups_members(top_groups[0]))
 
-print('\nGetting followers...')
 followers = get_followers_subquery(USER_ID)
 friends = get_friends(USER_ID)
 followers = followers + friends
@@ -257,33 +272,10 @@ followers_transformed = [{x['id']:{'sex': x['sex'], 'bdate':x['bdate']}}
                          if 'bdate' in x
                          else {x['id']:{'sex': x['sex'], 'bdate': 0}}
                          for x in followers]
-# print(followers_transformed[0:25])
-# followers_transformed=[{24067740: {'sex': 2, 'bdate': '18.1'}},
-#                        {223918359: {'sex': 2, 'bdate': '31.10.2001'}},
-#                        {340475094: {'sex': 1, 'bdate': 0}},
-#                        {249016476: {'sex': 2, 'bdate': '2.7'}},
-#                        {422707533: {'sex': 2, 'bdate': '16.4.1999'}},
-#                        {260562673: {'sex': 1, 'bdate': '8.3'}},
-#                        {357867508: {'sex': 2, 'bdate': '7.3.2003'}},
-#                        {251309909: {'sex': 2, 'bdate': '29.11.1976'}},
-#                        {422733096: {'sex': 1, 'bdate': 0}},
-#                        {422372294: {'sex': 1, 'bdate': 0}},
-#                        {320478535: {'sex': 1, 'bdate': '6.7'}},
-#                        {422719692: {'sex': 1, 'bdate': '20.6.1999'}},
-#                        {422726235: {'sex': 1, 'bdate': '28.6.1997'}},
-#                        {292742343: {'sex': 2, 'bdate': 0}},
-#                        {422722981: {'sex': 2, 'bdate': 0}},
-#                        {422725520: {'sex': 1, 'bdate': 0}},
-#                        {219348214: {'sex': 1, 'bdate': '9.5'}},
-#                        {422536678: {'sex': 2, 'bdate': 0}},
-#                        {377165906: {'sex': 1, 'bdate': '26.6.2003'}},
-#                        {151112749: {'sex': 1, 'bdate': '7.6'}},
-#                        {193973650: {'sex': 1, 'bdate': '15.8.1974'}},
-#                        {312738186: {'sex': 1, 'bdate': '26.7'}},
-#                        {134371027: {'sex': 2, 'bdate': '5.8'}},
-#                        {39078592: {'sex': 2, 'bdate': '14.2'}},
-#                        {385441315: {'sex': 1, 'bdate': 0}}]
-followers_ids = [list(x.keys())[0] for x in followers_transformed]
-print(get_users_groups_subquery(followers_transformed))
+#followers_ids = [list(x.keys())[0] for x in followers_transformed]
+test = get_users_groups_subquery(followers_transformed)
+print("\n", datetime.datetime.now())
+save_results(test, GROUPS_FILE)
+print("\n", datetime.datetime.now())
 #write_results(USERS_FILE, followers)
 
