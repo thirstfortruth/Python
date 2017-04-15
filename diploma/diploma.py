70d47b571bb8be29fc115098d6a98ece1ec55e28
thirstfortruth <thirstfortruth@yandex.ru>
2017-04-15 03:33:19 +0300

    Diploma. Simple plot

--- diploma/diploma.py
+++ diploma/diploma.py
@@ -3,8 +3,8 @@ from urllib.parse import urlencode, urlparse
 import requests
 import math
 import sys
-import datetime
 import pandas as pd
+import datetime
 import numpy as np
 
 AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
@@ -17,15 +17,9 @@ auth_data = {'client_id': APP_ID,
              'response_type': 'token',
              'scope': 'friends,status',
              'v': VERSION}
-print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
+# print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
 USER_ID = '80491907'
-token_url = 'https://oauth.vk.com/blank.html#access_token=c2a61400d4b928bc1ea7f35bc8d62b2d3f786b7168eb111d86cd5c3df6f3aaeb6235bab565819fbfb32d4&expires_in=86400&user_id=4931934'
-#while True:
-#    token_url = input('Please enter token URL')
-#    if len(token_url) == 0:
-#        print('Please enter URL')
-#        continue
-#    break
+token_url = 'https://oauth.vk.com/blank.html#access_token=4412584fcb24a791dfcaebd80bc4030b22b4b9511bdd17f8f5a3222ec8b8842f428ea80764236f17ee9ce&expires_in=86400&user_id=4931934'
 
 o = urlparse(token_url)
 fragments = dict((i.split('=') for i in o.fragment.split('&')))
@@ -45,106 +39,6 @@ def get_friends(user_id):
     get_params = {'access_token': access_token, 'user_id': user_id, 'v': VERSION, 'fields': "sex,bdate"}
     response = requests.get('https://api.vk.com/method/friends.get', get_params)
     return response.json()['response']['items']
-
-
-def get_groups_for_user(user_id):
-    params_groups_get = {'access_token': access_token,
-                         'user_id': user_id,
-                         'extended': 0,
-                         'v': VERSION}
-    response_groups_get = requests.get('https://api.vk.com/method/groups.get', params_groups_get)
-    return response_groups_get.json()
-
-
-def get_followers(user_id):
-    followers = []
-    count = 1000
-    params_followers_get = {'access_token': access_token,
-                            'user_id': user_id,
-                            'count': count,
-                            'v': VERSION}
-    response_followers_get = requests.get('https://api.vk.com/method/users.getFollowers', params_followers_get).json()
-    count_followers = response_followers_get['response']['count']
-    #count_followers = 10000
-    max_count = math.ceil(count_followers/count)*count
-    print('Total followers: ', max_count)
-    if count_followers > count:
-        for i in range(0, max_count, count):
-            progress_bar(i, max_count)
-            params_followers_get = {'access_token': access_token,
-                                    'user_id': user_id,
-                                    'count': count,
-                                    'offset': i,
-                                    'v': VERSION}
-            try:
-                response = requests.get('https://api.vk.com/method/users.getFollowers', params_followers_get).json()
-                time.sleep(0.35)
-                followers = followers + response['response']['items']
-            except:
-                time.sleep(0.35)
-        progress_bar(max_count, max_count)
-    return followers
-
-
-def get_followers_groups(followers, followers_count):
-    groups = []
-    count = 200
-    response_groups_get = None
-    progress_bar(0, followers_count)
-    if followers_count > count:
-        for i in range(count, followers_count, count):
-            progress_bar(i, followers_count)
-            try:
-                params_groups_get = {'access_token': access_token,
-                                     'user_id': followers[i-count: i],
-                                     'count': 1000,
-                                     'v': VERSION}
-                response_groups_get = requests.get('https://api.vk.com/method/groups.get', params_groups_get).json()
-                groups = groups + response_groups_get['response']['items']
-                time.sleep(0.3)
-            except:
-                time.sleep(0.3)
-    else:
-        try:
-            params_groups_get = {'access_token': access_token,
-                                 'user_id': followers,
-                                 'count': 1000,
-                                 'v': VERSION}
-            response_groups_get = requests.get('https://api.vk.com/method/groups.get', params_groups_get).json()
-            print(response_groups_get)
-            groups = groups + response_groups_get['response']['items']
-        except:
-            print("response_groups_get, response: ", response_groups_get)
-    progress_bar(followers_count, followers_count)
-    print("\n")
-    return groups
-
-
-def get_groups_members(group):
-    params_group_memebers = {'access_token': access_token,
-                         'group_id': group,
-                         'extended': 0,
-                         'v': VERSION}
-    response_group_memebers = requests.get('https://api.vk.com/method/groups.getMembers', params_group_memebers).json()
-    return response_group_memebers['response']['items']
-
-
-def write_results(filename, data_to_write):
-    with open(filename, 'a') as f:
-         for line in data_to_write:
-              f.write(str(line)+"\n")
-
-
-def read_file(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    return lines
-
-
-def get_top_list(input_list, limit):
-    df1 = pd.DataFrame({'Groups': input_list})
-    result = df1.groupby(['Groups']).size().reset_index().sort_values(by=0, ascending=False).head(limit)
-    return result['Groups'].values.tolist()
 
 
 def get_followers_subquery(user_id):
@@ -249,33 +143,42 @@ def save_results(details_to_save, file_to_save):
                                       str(group)
                     f.write(string_to_write+"\n")
 
-# print('\nGetting followers...')
-# # followers = get_followers(USER_ID)
-# # friends = get_friends(USER_ID)
-# # followers = followers + friends
-# # write_results(USERS_FILE, followers)
-# followers = read_file(USERS_FILE)
-#
-# print('\nGetting groups...')
-# # groups = get_followers_groups(followers,  len(followers))
-# # write_results(GROUPS_FILE, groups)
-# groups = read_file(GROUPS_FILE)
-#
-# top_groups = get_top_list(groups, 100)
-# print(get_groups_members(top_groups[0]))
 
-followers = get_followers_subquery(USER_ID)
-friends = get_friends(USER_ID)
-followers = followers + friends
-# print(followers[0])
-followers_transformed = [{x['id']:{'sex': x['sex'], 'bdate':x['bdate']}}
-                         if 'bdate' in x
-                         else {x['id']:{'sex': x['sex'], 'bdate': 0}}
-                         for x in followers]
-#followers_ids = [list(x.keys())[0] for x in followers_transformed]
-test = get_users_groups_subquery(followers_transformed)
-print("\n", datetime.datetime.now())
-save_results(test, GROUPS_FILE)
-print("\n", datetime.datetime.now())
+def sex_translate(num):
+    if num == 1:
+        char = 'F'
+    elif num == 2:
+        char = 'M'
+    else:
+        char = 'N'
+    return char
+
+
+def calculate_age(birthday):
+    b_date = datetime.datetime.strptime(birthday, '%d.%m.%Y')
+    age = math.floor(((datetime.datetime.today() - b_date).days / 365))
+    return age
+
+
+def main_cycle():
+    followers = get_followers_subquery(USER_ID)
+    friends = get_friends(USER_ID)
+    followers = followers + friends
+    followers_transformed = [{x['id']:{'sex': sex_translate(x['sex']), 'bdate':calculate_age(x['bdate'])}}
+                             if 'bdate' in x and x['bdate'].count('.') > 1
+                             else {x['id']:{'sex': sex_translate(x['sex']), 'bdate': 0}}
+                             for x in followers]
+    #followers_ids = [list(x.keys())[0] for x in followers_transformed]
+    # if limit:
+    #     details_with_groups = get_users_groups_subquery(followers_transformed[0:limit])
+    # else:
+    details_with_groups = get_users_groups_subquery(followers_transformed[0:1000])
+    print("\nSaving results", datetime.datetime.now())
+    save_results(details_with_groups, GROUPS_FILE)
+    print("\nDone saving!", datetime.datetime.now())
+
+
+main_cycle()
+
 #write_results(USERS_FILE, followers)
 
